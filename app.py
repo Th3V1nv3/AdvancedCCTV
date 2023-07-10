9a081cb28524f0c1e4a396e15dfaf68dd2a53311
Vincent Hlatshwayo <thevince30@gmail.com>
2023-07-10 11:58:21 +0200

    ###

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response
 from flask_sqlalchemy import SQLAlchemy
 import moviepy.editor as mp # this is a library for working with videos
 import random
+from fpdf import FPDF
+from datetime import date
 
 app = Flask(__name__)
 app.jinja_env.add_extension ('jinja2.ext.loopcontrols')
@@ -52,20 +54,53 @@ def upload():
         duration = clip.duration
         startTime = str(random.randint(0,59)) + ":" + str(random.randint(0, 59))
         accuracy = 94
+        id = 0
 
         metadata = Metadata(user_id = 1 , duration = "duration" , startTime = startTime , videoName = videoName ,searchKey = searchKey ,accuracy = accuracy)
-        #with app.app_context():
+        with app.app_context():
             #db.create_all()
-            #db.session.add(metadata)
-            #db.session.commit()
+            db.session.add(metadata)
+            db.session.commit()
+            db.session.refresh(metadata)
+            id = metadata.id
 
         # render the results page with the video information
-        return render_template('results.html', duration = duration , startTime = startTime , videoName = videoName ,searchKey = searchKey , accuracy = accuracy)
+        return render_template('results.html', duration = duration , startTime = startTime , videoName = videoName ,searchKey = searchKey , accuracy = accuracy ,id = id )
     else:
         # render the upload page with a form to upload a video
         return render_template('upload.html')
 
 @app.route('/history')
 def history():
+    metadataList = Metadata.query.all()
     # this is the home page of the website
-    return render_template('history.html')
+    return render_template('history.html',metadataList = metadataList)
+
+@app.route('/download/<int:id>', methods=['GET', 'POST'])
+def download(id):
+    metadata = Metadata.query.get_or_404(id)
+
+    pdf = FPDF()
+ 
+    # Add a page
+    pdf.add_page()
+
+    pdf.set_font("Arial", size = 15)
+
+    pdf.cell(200, 10, txt = "PDF analyser",ln = 1, align = 'R')
+    pdf.cell(200, 10, txt = "Video name "+metadata.videoName,ln = 4, align = 'L')
+    pdf.cell(200, 10, txt = "Duration "+str(metadata.duration),ln = 5, align = 'L')
+    pdf.cell(200, 10, txt = "Start time "+metadata.startTime,ln = 6, align = 'L')
+    pdf.cell(200, 10, txt = "Search key "+metadata.searchKey,ln = 7, align = 'L')
+    pdf.cell(200, 10, txt = "Accuracy "+str(metadata.accuracy),ln = 4, align = 'L')
+    #---------------------------------
+    pdf.output("output.pdf")  
+
+    # Return the PDF file as a response
+    response = make_response(open("output.pdf", "rb").read())
+    response.headers["Content-Type"] = "application/pdf"
+    response.headers["Content-Disposition"] = "attachment; filename=output.pdf"
+    return response
+    
+
+
